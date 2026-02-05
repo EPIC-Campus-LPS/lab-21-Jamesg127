814567ee6141a83de5a40775b3ff8f4aecb82876
Jamesg127 <jamesg127@lpsk12.org>
2026-02-05 11:29:26 -0700

    hello

--- src/ReversePolishNotation.java
+++ src/ReversePolishNotation.java
@@ -23,11 +23,27 @@ public class ReversePolishNotation {
         return s.peek();
     }
     public static String infixToPostfix(String input){
-        Stack s = new Stack();
+        Stack ns = new Stack();
+        Stack os = new Stack();
         String output = "";
         String numbers = "0123456789";
         for(int i = 0; i < input.length() - 1; i++){
-            if(!numbers.contains(input.substring(i, i + 1))){
+            if(numbers.contains(input.substring(i, i + 1))){
+                ns.push(Integer.parseInt(input.substring(i, i + 1)));
+            }
+            else{
+                if(input.substring(i, i + 1).equals("+")){
+                    os.push(0);
+                }
+                else if(input.substring(i, i + 1).equals("-")){
+                    os.push(1);
+                }
+                else if(input.substring(i, i + 1).equals("*")){
+                    os.push(2);
+                }
+                else if(input.substring(i, i + 1).equals("/")) {
+                    os.push(3);
+                }
 
             }
         }
