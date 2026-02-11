2912c98b9743032bea7656a63e077f118f2b20a5
Jamesg127 <jamesg127@lpsk12.org>
2026-02-11 11:58:39 -0700

    hello

---
+++ Tests/ReversePolishNotationTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReversePolishNotationTest {
+
+    @org.junit.jupiter.api.Test
+    void evaluatePostfix() {
+        String input = "72+9/35*-";
+        assertEquals(-14, ReversePolishNotation.evaluatePostfix(input));
+    }
+
+    @org.junit.jupiter.api.Test
+    void infixToPostfix() {
+        String input = "a+b*c+(d*e+f)*g";
+        assertEquals("abc*+de*f+g*+", ReversePolishNotation.infixToPostfix(input));
+    }
+
+}
--- src/ReversePolishNotation.java
+++ src/ReversePolishNotation.java
@@ -5,7 +5,11 @@ public class ReversePolishNotation {
         Stack s = new Stack();
         for(int i = 0; i < input.length() - 1; i++){
             if(input.substring(i, i + 1).equals("+")){
-                s.push(s.pop() + s.pop());
+                System.out.println("num" + s.peek());
+                int firstNum = s.pop();
+                System.out.println("num" + s.peek());
+                s.push(firstNum + s.pop());
+                System.out.println("result" + s.peek());
             }
             else if(input.substring(i, i + 1).equals("-")){
                 s.push(s.pop() - s.pop());
@@ -18,34 +22,86 @@ public class ReversePolishNotation {
             }
             else{
                 s.push(Integer.parseInt(input.substring(i, i + 1)));
+                System.out.println(s.peek());
             }
         }
         return s.peek();
     }
     public static String infixToPostfix(String input){
-        Stack ns = new Stack();
-        Stack os = new Stack();
+        Stack s = new Stack();
         String output = "";
         String numbers = "0123456789";
         for(int i = 0; i < input.length() - 1; i++){
             if(numbers.contains(input.substring(i, i + 1))){
-                ns.push(Integer.parseInt(input.substring(i, i + 1)));
+                output += input.substring(i, i + 1);
             }
             else{
                 if(input.substring(i, i + 1).equals("+")){
-                    os.push(0);
+                    while(s.peek() >= 0){
+                        if(s.peek() == 0){
+                            s.pop();
+                            output += "+";
+                        }
+                        else if(s.peek() == 1){
+                            s.pop();
+                            output += "-";
+                        }
+                        else if(s.peek() == 2){
+                            s.pop();
+                            output += "*";
+                        }
+                        else if(s.peek() == 3){
+                            s.pop();
+                            output += "/";
+                        }
+                    }
+                    s.push(0);
                 }
                 else if(input.substring(i, i + 1).equals("-")){
-                    os.push(1);
+                    while(s.peek() >= 0){
+                        if(s.peek() == 0){
+                            s.pop();
+                            output += "+";
+                        }
+                        else if(s.peek() == 1){
+                            s.pop();
+                            output += "-";
+                        }
+                        else if(s.peek() == 2){
+                            s.pop();
+                            output += "*";
+                        }
+                        else if(s.peek() == 3){
+                            s.pop();
+                            output += "/";
+                        }
+                    }
+                    s.push(1);
                 }
                 else if(input.substring(i, i + 1).equals("*")){
-                    os.push(2);
+                    s.push(2);
                 }
                 else if(input.substring(i, i + 1).equals("/")) {
-                    os.push(3);
+                    s.push(3);
                 }
-
+                else if(input.substring(i, i + 1).equals("(")){
+                    s.push(4);
+                }
+                else if(input.substring(i, i + 1).equals(")")){
+                    while(s.peek() < 4){
+                        if(s.peek() == 0){
+                            s.pop();
+                            output += "+";
+                        }
+                        if(s.peek() == 1){
+                            s.pop();
+                            output += "-";
+                        }
+                    }
+                }
             }
         }
+        return output;
     }
+
 }
